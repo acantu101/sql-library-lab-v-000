ea85a9664347f64d15020e6c46d23294b6c85487
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-11 21:15:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -43,7 +43,7 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
                                 "SELECT  characters.name, COUNT(DISTINCT(character_books.book_id))
-                        FROM character_books
+                                FROM character_books
                                 INNER JOIN characters
                                 ON character_books.character_id = chracters.id;"
 
