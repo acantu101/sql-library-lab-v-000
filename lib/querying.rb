ca045debcc9686931c678367259cc1e13dc26642
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-11 21:10:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -50,5 +50,5 @@ def select_character_names_and_number_of_books_they_are_in
                                 INNER JOIN characters
                                 ON character_books.character_id = chracters.id;"
 
-  
+
 end
