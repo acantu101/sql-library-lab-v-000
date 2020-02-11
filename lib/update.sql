712322d67a0f7385b073bd90d8b67b2e86c619e1
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-11 19:51:28 +0000

    Automatically backed up by Learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,1 +1,1 @@
-UPDATE characters SET species = "Martian" WHERE species = "Cylon" ORDER BY characters.id DESC LIMIT 1;
+UPDATE characters SET species = "Martian" WHERE species = "cylon" ORDER BY characters.id DESC LIMIT 1;
