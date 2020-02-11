c4f9bf8387d7ad6d5d5c7e2f26a36b8d5ee17430
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-11 20:07:54 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,6 +11,7 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto
   FROM characters
+  GROUP BY chractarer.id
   HAVING max(length(characters.motto))
   LIMIT 1;
   "
