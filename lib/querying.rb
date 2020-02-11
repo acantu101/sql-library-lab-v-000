c48210c6072a515c01455f59399eab58e2e67e2f
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-11 20:45:03 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,7 +28,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, (SELECT subgenres.name
                           FROM subgenres
                           JOIN series
-                          ON series.subgenres_id = subgenres.id
+                          ON series.subgenre_id = subgenres.id
                           GROUP BY subgenres.id)
   FROM authors
   JOIN series
