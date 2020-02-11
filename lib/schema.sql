5f04c2c0b287564b6a240e85905db26825c1425e
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-11 19:41:07 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,5 +1,5 @@
-CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, sub-genre_ID INTEGER);
-CREATE TABLE sub_genres (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
+CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year iNTEGER, series_id INTEGER);
 CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER);
